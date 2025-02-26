e061b2f3eae562ca4dbf5ae6808741319f0b412a
Naeem Maroof <naeemmaroof@gmail.com>
2025-02-25 22:15:20 -0500

    Create SimpleMath.java

---
+++ week6/SimpleMath.java
@@ -0,0 +1,10 @@
+package week6;
+
+public class SimpleMath {
+    public static int factorial(int n){
+      return 0;
+    }
+    public static int multiply2Ints(int x, int y){
+       return 0;
+    }
+}
